7d3c9a450603643df7b7b3d073ce20877279b13a
Faraj Farook <farajfarook@gmail.com>
2019-01-09 12:18:35 +1100

    Getting specific gpio

--- src/config/io.cpp
+++ src/config/io.cpp
@@ -3,6 +3,8 @@
 #include "config/_addr.h"
 #include "config/_constants.h"
 
+config_io_t *_io_cfg_cache = NULL;
+
 void config_io_set(config_io_t data)
 {    
     unsigned int addr = _IO_ADDR;
@@ -19,10 +21,14 @@ void config_io_save(config_io_t data)
 {
     config_io_set(data);
     Data.save();
+    _io_cfg_cache = NULL;
 }
 
 config_io_t config_io_get()
 {
+    if(_io_cfg_cache != NULL)
+        return *_io_cfg_cache;
+
     config_io_t data;
     unsigned int addr = _IO_ADDR;
     for(size_t i = 0; i < _IO_COUNT; i++)
@@ -33,6 +39,25 @@ config_io_t config_io_get()
         addr += _IO_LABEL_SIZE;
     }
     return data;
+}
+
+int config_gpio_index(String label)
+{
+    config_io_t data = config_io_get();
+    for(size_t i = 0; i < _IO_COUNT; i++)
+    {
+        if(data.gpio[i].label == label) 
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+config_gpio_t config_gpio_get(int index)
+{
+    config_io_t data = config_io_get();
+    return data.gpio[index];
 }
 
 config_io_t config_io_default() 
--- src/config/io.h
+++ src/config/io.h
@@ -19,5 +19,7 @@ config_io_t config_io_get();
 config_io_t config_io_default();
 void config_io_set(config_io_t data);
 void config_io_save(config_io_t data);
+int config_gpio_index(String label);
+config_gpio_t config_gpio_get(int index);
 
 #endif
